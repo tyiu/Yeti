62fe96bd2fa9606f6657863a136855c035e3be04
Terry Yiu <git@tyiu.xyz>
2025-01-19 21:56:37 -0500

    Add SwiftLint and fix lint warnings

--- YetiUITests/YetiUITestsLaunchTests.swift
+++ YetiUITests/YetiUITestsLaunchTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 final class YetiUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    override static var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
 
