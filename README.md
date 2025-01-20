4dc6a71a5c21ef383162198b0b4695388adf9a48
Terry Yiu <git@tyiu.xyz>
2025-01-19 21:38:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yeti: Nostr event signer for iOS
+
+Yeti is a Nostr event signer for iOS. It allows users to keep their private key segregated in a single, dedicated app.
---
+++ YetiTests/YetiTests.swift
@@ -0,0 +1,17 @@
+//
+//  YetiTests.swift
+//  YetiTests
+//
+//  Created by Terry Yiu on 1/19/25.
+//
+
+import Testing
+@testable import Yeti
+
+struct YetiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
